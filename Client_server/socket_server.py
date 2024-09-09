87b991ea1953a64a6565ece842b3552bf89fd56d
Колиберда Александр Дмитриевич <82934858+MrGreEe@users.noreply.github.com>
2024-09-09 16:36:17 +0300

    1

---
+++ Client_server/socket_server.py
@@ -0,0 +1,16 @@
+import socket
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind(('localhost', 3030))                    # Привязываем серверный сокет к localhost и 3030 порту.
+s.listen(1)                                    # Начинаем прослушивать входящие соединения
+conn, addr = s.accept()                        # Метод который принимает входящее соединение.
+
+while True:                         # Создаем вечный цикл.
+	data = conn.recv(1024)          # Получаем данные из сокета.
+	if not data:
+		break
+	conn.sendall(data)              # Отправляем данные в сокет.
+	print(data.decode('utf-8'))     # Выводим информацию на печать.
+conn.close()
