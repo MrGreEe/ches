f341ed3e9584c471041cc6c7ab7b65178836107e
MrGreEe <82934858+MrGreEe@users.noreply.github.com>
2024-09-21 21:13:51 +0300

    1

--- Client_server/socket_client.py
+++ Client_server/socket_client.py
@@ -1,9 +1,25 @@
+# import socket
+
+
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# s.connect(('localhost', 3030))                  # Подключаемся к нашему серверу.
+# s.sendall('Hello, Habr!'.encode('utf-8'))       # Отправляем фразу.
+# data = s.recv(1024)                             # Получаем данные из сокета.
+# s.close()
+
+
+
+# echo-client.py
+
 import socket
 
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b"Hello, world")
+    data = s.recv(1024)
 
-s.connect(('localhost', 3030))                  # Подключаемся к нашему серверу.
-s.sendall('Hello, Habr!'.encode('utf-8'))       # Отправляем фразу.
-data = s.recv(1024)                             # Получаем данные из сокета.
-s.close()
+print(f"Received {data!r}")
